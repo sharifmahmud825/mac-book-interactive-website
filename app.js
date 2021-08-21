cb212d4d3cbef7617461c9676ac42e22ae30b78d
unknown <sharifmahmud825@gmail.com>
2021-08-21 13:35:20 +0600

    Html file added

---
+++ app.js
@@ -0,0 +1,11 @@
+// Add event handler for memory buttons
+document.getElementById('8gb-memory-btn').addEventListener('click', function(){
+    const memoryInput = document.getElementById('memory-cost');
+    const memoryCostText = memoryInput.innerText;
+    const memoryCost = parseFloat(memoryCostText);
+    
+    
+})
+document.getElementById('256gb-memory-btn').addEventListener('click', function(){
+    
+})
