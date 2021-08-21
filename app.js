32069844a7839b150542b1c0a63612e5ec93162a
unknown <sharifmahmud825@gmail.com>
2021-08-21 14:42:15 +0600

    memory cost function added

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+
+function costOfMemory (type, price){
+
+    const getButtonValue = document.getElementById(`${type}-btn`);
+    const buttonValue = 1;
+// memory balance update
+    const memoryInput = document.getElementById('memory-cost');
+    memoryInput.innerText = buttonValue * price;
+}
+
+
+
 // Add event handler for memory buttons
 document.getElementById('8gb-memory-btn').addEventListener('click', function(){
-
-    const getValue = document.getElementById('8gb-memory-btn');
-    const value = 1;
-
-    const memoryInput = document.getElementById('memory-cost');
-    memoryInput.innerText = value * 0;
-    
-    
-    
+ 
+    costOfMemory ('8gb-memory', 0); 
 })
 document.getElementById('256gb-memory-btn').addEventListener('click', function(){
-    const getValue = document.getElementById('8gb-memory-btn');
-    const value = 1;
-
-    const memoryInput = document.getElementById('memory-cost');
-    memoryInput.innerText = value * 180;
+    
+    costOfMemory ('8gb-memory', 180);
 })
