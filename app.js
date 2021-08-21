229991f4acf921c1d00787e67499efbfc8e55ea2
unknown <sharifmahmud825@gmail.com>
2021-08-21 13:52:08 +0600

    functions added

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // Add event handler for memory buttons
 document.getElementById('8gb-memory-btn').addEventListener('click', function(){
+
+    const getValue = document.getElementById('8gb-memory-btn');
+    const value = 1;
+
     const memoryInput = document.getElementById('memory-cost');
-    const memoryCostText = memoryInput.innerText;
-    const memoryCost = parseFloat(memoryCostText);
+    memoryInput.innerText = value * 0;
+    
     
     
 })
 document.getElementById('256gb-memory-btn').addEventListener('click', function(){
-    
+    const getValue = document.getElementById('8gb-memory-btn');
+    const value = 1;
+
+    const memoryInput = document.getElementById('memory-cost');
+    memoryInput.innerText = value * 180;
 })
